f4234b9346d4c2de4e73b4077eeb73ad80f5dfcf
abaudat <granbaud@hotmail.com>
2025-09-02 18:35:37 +0200

    Change router base path

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename='/ai-kill-alice-web'>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/demo" element={<Demo />} />
